ad59d56ab64237306bac4d71a7fcb8ff51492448
FritzFlorian <flo.fritz@t-online.de>
2017-12-28 19:19:34 +0100

    Initial Commit - Page Setup

---
+++ js/main.js
@@ -0,0 +1,93 @@
+// Split Panerls
+// See: https://nathancahill.github.io/Split.js/
+Split(['#left', '#center', '#right'], {
+  sizes: [25, 50, 25]
+});
+
+// Drag & Drop panels
+// See: 
+$(function(){
+  $('#panel-parent').sortable();
+  $('#panel-parent').children().lobiPanel({
+    sortable: true,
+    reload: false,
+    unpin: false,
+    expand: false,
+    editTitle: false,
+  });
+
+
+  // Insert new nodes
+  var hidden = $('#hidden-panel');
+  hidden.removeAttr('id');
+
+  $('#add-button').click(function() {
+    var clone = hidden.clone();
+    clone.appendTo('#panel-parent');
+    clone.show();
+
+    $('#panel-parent').sortable("refresh");
+    $('#panel-parent').sortable("refreshPositions");
+
+    clone.lobiPanel({
+      sortable: true,
+      reload: false,
+      unpin: false,
+      expand: false,
+      editTitle: false,
+    });
+  });
+});
+
+// Experiment Browser
+// See: https://www.jstree.com/api/
+$('#experiment-browser').jstree({
+  'plugins' : ['contextmenu', 'dnd', 'wholerow'],
+  'core' : {
+    'check_callback' : true,
+    'multiple' : true,
+    'data' : [
+      {
+        "text" : "Interesting Findings",
+        "state" : { "opened" : true },
+        "children" : [
+          {
+            "text" : "Try more heat",
+            "state" : { "selected" : true },
+            "icon" : "jstree-file"
+          },
+          {
+            "text" : "Less heat, more salt",
+            "icon" : "jstree-file"
+          },
+        ]
+      }, {
+        "text" : "Old Experiments",
+        "state" : { "opened" : false },
+        "children" : [
+          {
+            "text" : "Try more heat",
+            "icon" : "jstree-file"
+          },
+          {
+            "text" : "Less heat, more salt",
+            "icon" : "jstree-file"
+          }, {
+            "text" : "Other Stuff",
+            "state" : { "opened" : false },
+            "children" : [
+              {
+                "text" : "Try more heat",
+                "icon" : "jstree-file"
+              },
+              {
+                "text" : "Less heat, more salt",
+                "icon" : "jstree-file"
+              },
+            ]
+          }
+        ]
+      }
+    ]
+  }
+});
