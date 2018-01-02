f5d861ae309bd4c515a693a194864851213ae908
FritzFlorian <flo.fritz@t-online.de>
2018-01-02 16:22:41 +0100

    Add proper file actions

--- js/main.js
+++ js/main.js
@@ -109,32 +109,25 @@ $('#experiment-browser').jstree({
     'multiple' : true,
     'data' : [
       {
-        "text" : "Interesting Findings",
+        "text" : "All Experiments",
         "state" : { "opened" : true },
         "children" : [
           {
-            "text" : "Try more heat",
-            "state" : { "selected" : true },
-            "icon" : "jstree-file"
-          },
-          {
-            "text" : "Less heat, more salt",
-            "icon" : "jstree-file"
-          },
-        ]
-      }, {
-        "text" : "Old Experiments",
-        "state" : { "opened" : false },
-        "children" : [
-          {
-            "text" : "Try more heat",
-            "icon" : "jstree-file"
-          },
-          {
-            "text" : "Less heat, more salt",
-            "icon" : "jstree-file"
+            "text" : "Interesting Findings",
+            "state" : { "opened" : true },
+            "children" : [
+              {
+                "text" : "Try more heat",
+                "state" : { "selected" : true },
+                "icon" : "jstree-file"
+              },
+              {
+                "text" : "Less heat, more salt",
+                "icon" : "jstree-file"
+              },
+            ]
           }, {
-            "text" : "Other Stuff",
+            "text" : "Old Experiments",
             "state" : { "opened" : false },
             "children" : [
               {
@@ -144,14 +137,147 @@ $('#experiment-browser').jstree({
               {
                 "text" : "Less heat, more salt",
                 "icon" : "jstree-file"
-              },
+              }, {
+                "text" : "Other Stuff",
+                "state" : { "opened" : false },
+                "children" : [
+                  {
+                    "text" : "Try more heat",
+                    "icon" : "jstree-file"
+                  },
+                  {
+                    "text" : "Less heat, more salt",
+                    "icon" : "jstree-file"
+                  },
+                ]
+              }
             ]
           }
         ]
-      }
-    ]
+      },
+    ],
+  },
+  'contextmenu': {
+    'items' : function (o, cb) { // Could be an object directly
+      result =  {
+        "rename" : {
+          "separator_before"  : false,
+          "separator_after" : false,
+          "_disabled"     : false, //(this.check("rename_node", data.reference, this.get_parent(data.reference), "")),
+          "label"       : "Rename",
+          /*!
+          "shortcut"      : 113,
+          "shortcut_label"  : 'F2',
+          "icon"        : "glyphicon glyphicon-leaf",
+          */
+          "action"      : function (data) {
+            var inst = $.jstree.reference(data.reference),
+              obj = inst.get_node(data.reference);
+            inst.edit(obj);
+          }
+        },
+        "remove" : {
+          "separator_before"  : false,
+          "icon"        : false,
+          "separator_after" : false,
+          "_disabled"     : false, //(this.check("delete_node", data.reference, this.get_parent(data.reference), "")),
+          "label"       : "Delete",
+          "action"      : function (data) {
+            var inst = $.jstree.reference(data.reference),
+              obj = inst.get_node(data.reference);
+            if(inst.is_selected(obj)) {
+              inst.delete_node(inst.get_selected());
+            }
+            else {
+              inst.delete_node(obj);
+            }
+          }
+        },
+        "clone" : {
+          "separator_before"  : false,
+          "icon"        : false,
+          "separator_after" : false,
+          "label"       : "Clone",
+          "action"      : function (data) {
+            clone();
+          }
+        },
+        "ccp" : {
+          "separator_before"  : true,
+          "icon"        : false,
+          "separator_after" : false,
+          "label"       : "Edit",
+          "action"      : false,
+          "submenu" : {
+            "cut" : {
+              "separator_before"  : false,
+              "separator_after" : false,
+              "label"       : "Cut",
+              "action"      : function (data) {
+                var inst = $.jstree.reference(data.reference),
+                  obj = inst.get_node(data.reference);
+                if(inst.is_selected(obj)) {
+                  inst.cut(inst.get_top_selected());
+                }
+                else {
+                  inst.cut(obj);
+                }
+              }
+            },
+            "copy" : {
+              "separator_before"  : false,
+              "icon"        : false,
+              "separator_after" : false,
+              "label"       : "Copy",
+              "action"      : function (data) {
+                copy();
+              }
+            },
+            "paste" : {
+              "separator_before"  : false,
+              "icon"        : false,
+              "_disabled"     : function (data) {
+                return !$.jstree.reference(data.reference).can_paste();
+              },
+              "separator_after" : false,
+              "label"       : "Paste",
+              "action"      : function (data) {
+                paste();
+              }
+            }
+          }
+        }
+      };
+
+
+      return result;
+    }
   }
 });
+
+// Copy/Past of context menu
+function copy() {
+  $('#experiment-browser').jstree().copy();
+}
+
+function paste() {
+  pasteTarget = $('#experiment-browser').jstree().get_selected();
+  if ($('#experiment-browser').jstree().get_icon(pasteTarget) == 'jstree-file') {
+    pasteTarget = $('#experiment-browser').jstree().get_parent(pasteTarget);
+  }
+
+  $('#experiment-browser').jstree().paste(pasteTarget);
+}
+
+function clone() {
+  copy();
+  paste();
+  $('#experiment-browser').jstree().edit($('#experiment-browser').jstree().get_selected());
+}
+
+function delete_node() {
+  $('#experiment-browser').jstree().delete_node($('#experiment-browser').jstree().get_selected());
+}
 
 //Parm Panel
 $(function(){  
